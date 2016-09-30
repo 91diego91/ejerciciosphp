5be88c3ff47608ef988a55d70b4da1157956be08
Diego Palomera <diegopalomera4@gmail.com>
2016-09-30 18:21:23 +0200

    commitmio

--- ejercicio01.php
+++ ejercicio01.php
@@ -1,1 +1,17 @@
-TODO:
+<!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>ejemplo 4</title>
+</head><body>
+<?php
+
+echo "Array ordenado inicializado en la declaración <hr>";
+$array = array(1,"elemento2", 3.3, true, false, "elemento5");
+
+//bucle foreach:
+echo "<hr> Recorro el array con un bucle foreach simple <br>";
+foreach ($array as $element){
+    echo $element . '<br>';
+}
+?>
+</body></html>
